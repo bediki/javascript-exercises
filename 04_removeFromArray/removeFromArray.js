18e6d2b8ee400a14f4a8c7a854aefb92ce070c19
bediki <bedi.kilic@gmail.com>
2022-12-25 20:28:01 +0100

    finished exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(firstArray, ...theArgs) {
+    for (const element of theArgs) {
+        for (const number of firstArray) {
+            if(number === element){
+                const index = firstArray.indexOf(element);
+                const x = firstArray.splice(index, 1);
+            }
+        }
+    }
+    return firstArray;
 };
+
+removeFromArray([1, 2, 3, 4], 1, 2, 4)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
