f525679c1df8ee3478f98152fd3737454eec32b5
bediki <bedi.kilic@gmail.com>
2022-12-25 20:44:54 +0100

    Implement gaussche summenregel

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(start <0 | end <0) return "ERROR with negative numbers'"
+
+    let sumOfAll = end*(end+1)/2;
+    return sumOfAll;
 };
 
 // Do not edit below this line
