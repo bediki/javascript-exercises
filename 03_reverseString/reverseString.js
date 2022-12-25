00606c7f5a6f7447b089df8f66891af8daeb8f69
bediki <bedi.kilic@gmail.com>
2022-12-25 15:49:46 +0100

    Implement split and join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let stringArray = string.split('');
+    console.log(stringArray.reverse().join(""));
 
 };
+
+reverseString("hello world");
 
 // Do not edit below this line
 module.exports = reverseString;
