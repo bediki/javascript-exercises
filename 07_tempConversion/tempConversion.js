89aa726d9c8a8c39852992da1e04a4d336982cfb
bediki <bedi.kilic@gmail.com>
2022-12-29 18:30:07 +0100

    Implemnt formulas and rounding numbers on one decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  celsius = ((temperature-32)*5/9);
+  return Math.round(celsius * 10) / 10 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // Convert to Fahrenheit
+  fahrenheit = (temperature*9/5+32)
+  // Round the number
+  return Math.round(fahrenheit * 10) / 10 
 };
 
 // Do not edit below this line
